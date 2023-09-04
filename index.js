37b00f50764309d57639760a3fcb9c8bea965c8f
jeonga214 <jeonga020214@gmail.com>
2023-09-04 10:12:36 +0900

    jsonserver

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const bodyParser = require('body-parser')
+const app = express();
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/abc', function (req, res) {
+  const jsonData = fs.readFileSync('./test.json')
+  res.send(  JSON.parse(jsonData)  )
+});
+
+app.get('/abc/:id', function (req, res) {
+  const jsonData = fs.readFileSync('./test.json');
+  const data = JSON.parse(jsonData);
+  
+  const {id} = req.params;
+  const aaa = data.filter( n=>n.id == id )
+  res.send( aaa )
+});
+
+app.post('/insert', function (req, res) {
+  fs.writeFileSync('./test.json', JSON.stringify(req.body) );
+  res.send('성공');
+});
+
+app.listen(3030)
